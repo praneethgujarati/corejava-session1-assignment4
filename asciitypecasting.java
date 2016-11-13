ad3b18cfded49e996c4eac88aa61ebf76efaae59
praneethgujarati <praneeth705@gmail.com>
2016-11-13 22:51:18 +0530

    Add files via upload

---
+++ asciitypecasting.java
@@ -0,0 +1,36 @@
+class asciitypecasting
+{
+   public static void main(String args[])
+     {
+       int a=65,b=66,c=67,d=68,e=69,f=70,g=71,h=72,i=73,j=74,k=75,l=76,m=77,n=78,o=79,p=80,q=81,r=82,s=83,t=84,u=85,v=86,w=87,x=88,y=89,z=90;
+       char ch1=(char)a,ch2=(char)b,ch3=(char)c,ch4=(char)d,ch5=(char)e,ch6=(char)f,ch7=(char)g,ch8=(char)h,ch9=(char)i,ch10=(char)j,ch11=(char)k,ch12=(char)l,ch13=(char)m,ch14=(char)n,ch15=(char)o,ch16=(char)p,ch17=(char)q,ch18=(char)r,ch19=(char)s,ch20=(char)t,ch21=(char)u,ch22=(char)v,ch23=(char)w,ch24=(char)x,ch25=(char)y,ch26=(char)z;
+       System.out.println("The ASCII code and its corresponding values are:");
+       System.out.println(+a + "-"  +ch1);
+       System.out.println(+b + "-"  +ch2);
+       System.out.println(+c + "-"  +ch3);
+       System.out.println(+d + "-"  +ch4);
+       System.out.println(+e + "-"  +ch5);
+       System.out.println(+f + "-"  +ch6);
+       System.out.println(+g + "-"  +ch7);
+       System.out.println(+h + "-"  +ch8);
+       System.out.println(+i + "-"  +ch9);
+       System.out.println(+j + "-"  +ch10);
+       System.out.println(+k + "-"  +ch11);
+       System.out.println(+l + "-"  +ch12);
+       System.out.println(+m + "-"  +ch13);
+       System.out.println(+n + "-"  +ch14);
+       System.out.println(+o + "-"  +ch15);
+       System.out.println(+p + "-"  +ch16);
+       System.out.println(+q + "-"  +ch17);
+       System.out.println(+r + "-"  +ch18);
+       System.out.println(+s + "-"  +ch19);
+       System.out.println(+t + "-"  +ch20);
+       System.out.println(+u + "-"  +ch21);
+       System.out.println(+v + "-"  +ch22);
+       System.out.println(+w + "-"  +ch23);
+       System.out.println(+x + "-"  +ch24);
+       System.out.println(+y + "-"  +ch25);
+       System.out.println(+z + "-"  +ch26);
+
+     }
+}
